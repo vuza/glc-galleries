403e3c40d48cf9c73741a93d587f9192b62e92a1
Marlon Alagoda <marlon@alagoda.at>
2016-05-01 19:24:27 +0200

    fix filter default return

--- templates/filter.php
+++ templates/filter.php
@@ -1,17 +1,21 @@
 <?php
 
 add_filter( 'archive_template', 'loadArchiveTemplate' );
-function loadArchiveTemplate(){
+function loadArchiveTemplate($archive_template){
     if(get_post_type() == 'glc_gallery'){
         $template = MYPLUGIN_FOLDER . 'templates/archive-glc_gallery.php';
         return $template;
     }
+
+    return $archive_template;
 }
 
 add_filter( 'single_template', 'loadGalleryTemplate');
-function loadGalleryTemplate(){
+function loadGalleryTemplate($single_template){
     if(get_post_type() == 'glc_gallery'){
         $template = MYPLUGIN_FOLDER . 'templates/post-glc_gallery.php';
         return $template;
     }
+
+    return $single_template;
 }

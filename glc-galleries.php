c41990ac85479c63ddaebd8a0078f2d61d435225
Marlon Alagoda <marlon@alagoda.at>
2016-05-01 19:24:47 +0200

    version bump

--- glc-galleries.php
+++ glc-galleries.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: GLC Galleries
  * Description: Event galleries for GLC
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: Marlon Alagoda, Niki Themmer
  * License: All private
  */
